0fbaec5ce4b64c2b1fda1db26a849b48789391ed
João Pedro <joao.girardello@khomp.com>
2025-07-21 16:59:12 -0300

    commit da aula 2: switch case

---
+++ aula_2_switch/aula_2.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("\nMENU:");
+Console.WriteLine("1 - Ver produtos");
+Console.WriteLine("2 - Fazer pedido");
+Console.WriteLine("0 - Sair do programa");
+Console.WriteLine("Escolha uma opção: ");
+
+int opcao = int.Parse(Console.ReadLine());
+
+
+switch (opcao)
+{
+    case 1:
+        Console.WriteLine("Pedido realizado");
+        break;
+    case 2:
+        Console.WriteLine("Pedido realizado");
+        break;
+    case 0:
+        Console.WriteLine("Saindo do programa...");
+        break;
+    default:
+        Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+        break;
+}
